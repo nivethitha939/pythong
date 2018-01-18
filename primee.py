e8fcdc41bb5c6b9deaa1944ff0fc843d8a03d2ee
akilsiva <30828660+akil1998@users.noreply.github.com>
2018-01-18 16:41:00 +0530

    Create primee.py

---
+++ primee.py
@@ -0,0 +1,9 @@
+a=input("enter the values:")
+b=input("enter the values:")
+for i in range(a,b + 1):
+   if i > 1:
+       for j in range(2,i):
+           if (i % j) == 0:
+               break
+       else:
+           print(i)
