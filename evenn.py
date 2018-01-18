5e7f82aa774ed2cd1b66fa2ccdc78c5d7c1df070
akilsiva <30828660+akil1998@users.noreply.github.com>
2018-01-18 16:34:32 +0530

    Create evenn.py

---
+++ evenn.py
@@ -0,0 +1,8 @@
+a=input("enter the values:")
+b=input("enter the values:")
+for i in range(a,b):
+   if i >=1:
+       for j in range(1,i):
+           if (i % 2) == 0:
+               print(i)
+      
