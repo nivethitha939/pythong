fc2be21f48f0fce126c5e454372a213c61c17417
akilsiva <30828660+akil1998@users.noreply.github.com>
2018-01-19 08:38:29 +0000

    Update couspa.py

--- couspa.py
+++ couspa.py
@@ -1,1 +1,10 @@
-
+a = input("Enter file name: ")
+k = 0
+ with open(a, 'r') as f:
+    for line in f:
+        b = line.split()
+        for i in b:
+            for c in i:
+                if(c.isspace):
+                    k=k+1
+print(k)
