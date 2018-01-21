ab53611236f7f9f296ccbff8dd5e388cca77b867
akilsiva <30828660+akil1998@users.noreply.github.com>
2018-01-21 12:07:19 +0530

    Create intstr.py

---
+++ intstr.py
@@ -0,0 +1,21 @@
+a=input("enter the number between 1 to 10:")
+if (a==0):
+	print("zero")
+if (a==1):
+	print("one")
+if (a==2):
+	print("two")
+if (a==3):
+	print("three")
+if (a==4):
+	print("four")
+if (a==5):
+	print("five")
+if (a==6):
+	print("six")
+if (a==7):
+	print("seven")
+if (a==8):
+	print("eight")
+if (a==9):
+	print("nine")
