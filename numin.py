01ff00ffc5310d1ee9bfc04310259948c589a45c
akilsiva <30828660+akil1998@users.noreply.github.com>
2018-01-23 16:54:48 +0530

    Create numin.py

---
+++ numin.py
@@ -0,0 +1,7 @@
+a=raw_input("Enter a number:")
+b=list(a)
+if ("1" in b)or("2" in b)or("3" in b)or("4" in b)or("5" in b)or("6" in b)or("7" in b)or("8" in b)or("9" in b)or("0" in b):
+    print("yes")
+else:
+    print("no")
+
