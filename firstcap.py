26b02246768470c448e4d867310d0c7f8f687c41
akilsiva <30828660+akil1998@users.noreply.github.com>
2018-02-27 16:55:31 +0530

    Update firstcap.py

--- firstcap.py
+++ firstcap.py
@@ -9,10 +9,11 @@ while c>0:
     d=list(b[i])
     e=d[0].upper()
     h=len(d)
+    h=h-1
     d[0]=e
     k=1
-    while h-1>0:
-        d[k].lower()
+    while h>0:
+        d[k]=d[k].lower()
         k=k+1
         h=h-1
     print("".join(d)," ",end="")
