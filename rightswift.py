7d6f8482c5cbd2827936359c8a5cab740b94cebd
akilsiva <30828660+akil1998@users.noreply.github.com>
2018-03-16 14:18:14 +0530

    Create rightswift.py

---
+++ rightswift.py
@@ -0,0 +1,25 @@
+a=int(input())
+c=a
+d=c
+i=d
+j=i
+b=int(input())
+f=[]
+while a>0:
+    f.append(input())
+    a=a-1
+f.insert(d,0)
+while b>0:
+    i=j
+    d=i
+    c=d
+    e=c
+    h=e
+    while c>0:
+        f[d]=f[i-1]
+        d=d-1
+        i=i-1
+        c=c-1
+    f[0]=f[e]
+    b=b-1
+print(" ".join(f[:-1]))        
