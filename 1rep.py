8fbc42b195905e696b2842837a20af746037047c
akilsiva <30828660+akil1998@users.noreply.github.com>
2018-07-16 22:38:53 +0530

    Update 1rep.py

--- 1rep.py
+++ 1rep.py
@@ -1,6 +1,12 @@
-a,b,c=int(input()),list(str(input()).split()),[]
-for i in range(0,a):
+a,b,c=int(input()),input().split(),[]
+for i in range(a):
     for j in range(i+1,a):
         if b[i]==b[j]:
             c.append(b[i])
-print(c[0])
+            break
+    if len(c)==1:
+        break
+if len(c)==0:
+    print("unique")
+else:
+    print(" ".join(c))
