ac5692678ef1cf81195448252341110541abd598
akilsiva <30828660+akil1998@users.noreply.github.com>
2018-07-16 22:22:48 +0530

    Update equalindex.py

--- equalindex.py
+++ equalindex.py
@@ -2,4 +2,7 @@ a,b,c=int(input()),list(str(input()).split()),[]
 for i in range(0,a):
     if int(i) == int(b[i]):
         c.append(str(b[i]))
-print(" ".join(sorted(c)))
+if len(c)==0:
+    print("-1")
+else:
+    print(" ".join(sorted(c)))
