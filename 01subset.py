bad7aea94a27df293bf51458db9cb877bf0e17df
akilsiva <30828660+akil1998@users.noreply.github.com>
2018-07-17 22:09:27 +0530

    Update 01subset.py

--- 01subset.py
+++ 01subset.py
@@ -1,4 +1,4 @@
-a,b,c,d,e=int(input()),int(input()),input().split(),input().split(),0
-for i in range(b):
+a,c,d,e=input().split(),input().split(),input().split(),0
+for i in range(int(a[1])):
     if d[i] in c:e=e+1
-print("YES" if e==b else "NO")
+print("YES" if e==int(a[1]) else "NO")
